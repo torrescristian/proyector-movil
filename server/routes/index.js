f9bc29f9fea32ec864eb74ec50025151ce139936
Cristian Torres <cristian.torres@naranja.com>
2019-06-07 15:07:38 -0300

    Quitado el /profesor del archivo index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,5 @@ const api = require('./api.routes');
 const path = require('path');
 
 router.use('/api', api);
-router.get('/profesor', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'submodules', 'proyector-movil-teacher', 'dist', 'index.html'));
-});
 
 module.exports = router;
